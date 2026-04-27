6f7ef8b5c06e4347f3f1e69614b6a480b392e032
ankeshchaubey6-ai <ankeshchaubey6@gmail.com>
2026-04-27 09:47:10 +0530

    fix

--- backend/src/core/app.js
+++ backend/src/core/app.js
@@ -32,7 +32,8 @@ app.use(
 /* =========================================================
    MIDDLEWARE
 ========================================================= */
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
